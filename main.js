d1c927af4b9f71a0246c41bc9de138b42de52f1d
DFam1411 <jared14111997@gmail.com>
2023-02-14 16:09:40 +0100

    commit one

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 console.log('test');
 const header = document.getElementById('first-header');
 header.innerText = 'Main 1111'
-header.style.color = 'purple'
+header.style.color = 'green'
 console.log('Main header', header);
+
+const element1 = document.getElementById('lists');
+console.log('element1 is', element1);
