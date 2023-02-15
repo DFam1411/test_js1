373edfc5d5b63c1178c6e1be8dd198a8215fe292
DFam1411 <jared14111997@gmail.com>
2023-02-15 21:02:55 +0100

    second project

--- testjs2/main.js
+++ testjs2/main.js
@@ -1,6 +1,11 @@
 const id = document.getElementById('user_id');
 console.log(id)
 
+const signInObject = {
+    username: ``,
+    email: ``,
+    password: ``
+}
 function formSubmitted(){
     if(id.value.length==0){
         id.style.borderColor = 'red';
