064fbaba037d1ee8d78d825b6ca12558b2059a67
DFam1411 <jared14111997@gmail.com>
2023-02-24 23:37:22 +0100

    1

--- testjs2/main.js
+++ testjs2/main.js
@@ -28,30 +28,42 @@ function checkUsername() {
         uName.style.font = 'Arial';
     }
 }
+/*
+function ValidateEmail()
+{
+    const mailValidate = document.getElementById('email');
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mailValidate.value))
+    {
+        return (true)
+    }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
+ */
 function checkPass(){
     const pass = document.getElementById('passID');
     const colorOK = "#AAEBA8";
     const colorNotOK = "red";
     const message = document.getElementById('error');
+    const format = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
     if(pass.value.length ==0){
         pass.style.backgroundColor = '#F6F0F8'
     }
+        if (pass.value.length >= 8) {
+            pass.style.backgroundColor = colorOK;
+            pass.style.color = '#000'
+            pass.style.font = 'large';
+            message.style.color = '#000';
+            message.style.font = 'large';
+            message.innerHTML = "password is strong enough";
+        } else {
+            pass.style.color = colorNotOK;
+            message.style.color = '#000';
+            message.style.font = 'large';
+            message.innerHTML = 'please, make sure your password is at least 8 symbols';
+        }
+    }
 
-    if(pass.value.length >= 8){
-        pass.style.backgroundColor = colorOK;
-        pass.style.color = '#000'
-        pass.style.font = 'large';
-        message.style.color = '#000';
-        message.style.font = 'large';
-        message.innerHTML="password is strong enough";
-    }
-    else {
-        pass.style.color = colorNotOK;
-        message.style.color = '#000';
-        message.style.font = 'large';
-        message.innerHTML = 'please, make sure your password is at least 8 symbols';
-    }
-}
 function usernameStoring() {
     let username1 = document.getElementById('user_id').value;
     alert(username1);
